fc347b10db76226bd6a92106f803706885e92e7b
lepisma <abhinav.tushar.vs@gmail.com>
2014-01-26 08:43:02 +0530

    Adds levels

--- server/score_calculator.py
+++ server/score_calculator.py
@@ -1,6 +1,17 @@
 import cv2
 import numpy as np
 
-def findScore(level_id, points):
-	cv2.
+def parsePoints(webPoints):
+	return # Points for cv use
+
+def findScore(level_id, points, dominant_color):
+	fileref = "./levels/" + str(level_id) + ".jpg"
+	mask = cv2.imread("mask.jpg")
+	mask = cv2.line(mask, (10, (mask.shape[0] - 10)), (points[0]), (255, 255, 255), 20)
+	mask = cv2.line(mask, points[0], points[1], (255, 255, 255), 20)
+	mask = cv2.line(mask, points[1], points[2], (255, 255, 255), 20)
+	mask = cv2.line(mask, points[2], ((mask.shape[1] - 10), 10), (255, 255, 255), 20)
+
+	img = cv2.imread(fileref)
+	result = cv2.bitwise_and(img, img, mask = mask)
 	return # Score
